afeea203842ce278d1f6b9515d9a21e81e2460d9
AZAD <mandariazad6666@gmail.com>
2024-01-17 17:54:00 -0800

    Initial Commit

---
+++ azad/azadApp.swift
@@ -0,0 +1,17 @@
+//
+//  azadApp.swift
+//  azad
+//
+//  Created by user240113 on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct azadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
